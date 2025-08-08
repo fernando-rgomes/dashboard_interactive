a584cd36a2769afae6e6e1d3f8c6b9c06af0d93a
Fernando <166315110+fernando-rgomes@users.noreply.github.com>
2025-08-08 14:58:38 -0300

    Create app.py and add data file and requirements

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# --- Configuração da página --- #
+# Define o título da página, o ícone e o layout para ocupar a largura inteira
+
+st.set_page_config(
+    page_title="🎲Dahboards de Salários na Área de Dados",
+    page_icon="📊",
+    layout="wide",
+)
+
+# --- Carregamento dos dados ---
+df.read_csv("")
